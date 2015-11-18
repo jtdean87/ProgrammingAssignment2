131d0ba840f982f18ad9b03adcb151be5adad10d
Tyler Dean <git.jtd@gmail.com>
2015-11-18 11:11:36 -0600

    First commit

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,37 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        iX <- NULL
+        set <- function(y){
+                x <<- y
+                iX <<- NULL
+        }
+        get <- function(){
+                x
+        }
+        inverted <- function(x) {
+                iX <<- solve(x)
+                iX
+        }
+        getinverted <- function(){
+                iX
+        }
+        list(
+                set=set,
+                inverted = inverted, 
+                get=get,
+                getinverted = getinverted
+        )
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        nX <- x$getinverted
+        if (!is.null(nX()))
+        {
+                message("cached data")
+                return(nX())
+        }
+        m <- x$get()
+        nX <- x$inverted(m)
+        nX
 }
